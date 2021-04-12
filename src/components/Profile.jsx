0a0f2ae65a0bf1ca2b9e8057d5b13d80494eaf54
MaksimLuzhynski <60569114+MaksimLuzhynski@users.noreply.github.com>
2021-04-12 16:16:35 +0300

    14 (css-modules)

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,10 @@
+import classes from './Profile.module.css';
+
 export const Profile = () => {
     return (
-        <div className="content">
+        <div className={classes.profile}>
             <div>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_q2SeFBoYK96BQmgIts3qwo0sS_l0o0-wlw&usqp=CAU"></img>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6IkOfF9qPvJbdtc9-Cusnip1k4AGuUytvtg&usqp=CAU"></img>
             </div>
             <div>
                 avatar + discription
@@ -11,12 +13,12 @@ export const Profile = () => {
                 My posts
                 <div>
                     New posts
-                    <div>
-                        <div>post1</div>
-                        <div>post2</div>
-                        <div>post3</div>
-                        <div>post4</div>
-                        <div>post5</div>
+                    <div className={classes.posts}>
+                        <div className={classes.item}>post10000</div>
+                        <div className={classes.item}>post2</div>
+                        <div className={classes.item}>post3</div>
+                        <div className={classes.item}>post4</div>
+                        <div className={classes.item}>post5</div>
                     </div>
                 </div>
             </div>
