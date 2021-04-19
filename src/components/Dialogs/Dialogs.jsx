5a780d26ea6c3b1114f3f716e15e858be8ef8525
MaksimLuzhynski <60569114+MaksimLuzhynski@users.noreply.github.com>
2021-04-19 14:58:20 +0300

    21 (page Messages)

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,19 @@ import classes from './Dialogs.module.css';
 
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog}>Sveta</div>
+                <div className={classes.dialog}>Victor</div>
+                <div className={classes.dialog}>Bob</div>
+                <div className={classes.dialog}>Max</div>
+                <div className={classes.dialog}>Sasha</div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi</div>
+                <div className={classes.message}>Hellow</div>
+                <div className={classes.message}>Yo</div>
+            </div>
         </div>
     )
 }
